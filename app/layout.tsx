405ced9c7ca47c95abfa1ccd7c31bc381438f0e1
Oscar-Kvissberg <oscarkvissberg@gmail.com>
2025-03-20 15:41:09 +0100

    kajosd

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="light">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </head>
       <body className={`${inter.className} bg-gradient-to-b from-gray-50 to-white min-h-screen`}>
         <NavBar />
         {children}
