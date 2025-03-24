9546f9b074981734209513a1f37eef85e6987395
Oscar-Kvissberg <oscarkvissberg@gmail.com>
2025-03-24 18:38:44 +0100

    jsdljf

--- app/[club]/MailGeneration/page.tsx
+++ app/[club]/MailGeneration/page.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import OpenAIBot from '../../components/OpenAIBot'
-import { clubs } from '../../config/clubs'
 
 type Props = {
   params: Promise<{ club: string }>
 }
 
 export default async function MailGenerationPage({ params }: Props) {
-  const { club } = await params
-  const clubConfig = clubs[club] || clubs.vasatorp
+  await params
   
   return (
     <div className="container mx-auto px-4 py-8">
